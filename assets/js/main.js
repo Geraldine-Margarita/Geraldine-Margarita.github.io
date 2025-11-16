7c24ab2974a37b01b4c1fbcbfa01c65d0873bdf5
Geraldine Bravo <134558998+Geraldine-Margarita@users.noreply.github.com>
2025-11-16 20:02:05 -0300

    Update API URL and local series path

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://mindicador.cl/api';
-const LOCAL_SERIES = '../mindicador.json'; 
+const LOCAL_SERIES = 'https://geraldine-margarita.github.io/Prueba-Api/mindicador.json'; 
 
 const $ = s => document.querySelector(s);
 const montoInput = $('#monto');
@@ -107,3 +107,4 @@ return;
 const { labels, values } = prepararSerie(historial[moneda]);
 dibujarGrafico(labels, values, valorActual.nombre);
 });
+
